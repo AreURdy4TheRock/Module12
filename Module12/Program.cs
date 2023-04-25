acd21e98e3efecaf52180c436c35448e76012ea1
AreURdy4TheRock <dorkwingdock@gmail.com>
2023-04-25 18:43:01 +0300

    Добавил основную логику работы программы.

--- Module12/Program.cs
+++ Module12/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Module12
+﻿using Module12.Services;
+
+namespace Module12
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<User> people = new List<User>(); // Создаём и заполняем список пользователей
+            people.Add(new User("login", "Артём", true ));
+            people.Add(new User("login1", "Максим", false));
+            people.Add(new User("login", "Михаил", true));
+            people.Add(new User("login", "Светлана", false));
+
+            foreach (User user in people) // Для каждого пользователя из списка
+            {
+                Show.Greetengs(user.Name); // Выводим приветствие
+                if (!user.IsPremium) // Если нет премиум аккаунта
+                {
+                    Show.ShowAds(); // Показываем рекламу
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
---
+++ Module12/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module12
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+        public User(string login, string name, bool ispremium) // Конструктор
+        { 
+            Login = login;
+            Name = name;
+            IsPremium = ispremium;        
+        }
+    }
+}
